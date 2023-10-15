17e52dfeafa841013fb9861afe946cc630755dab
Shrey Shrivastav <tetrex@shrey-pc.localdomain>
2023-10-15 22:48:56 +0530

    updated request handler

--- server.sh
+++ server.sh
@@ -11,17 +11,27 @@ rm -f response && mkfifo response
 # 2) Route request to the correct handler
 # 3) Build a response based on the request
 # 4) Send the response to the named pipe (FIFO)
-handleRequest() {
+requestHandler() {
   while read line; do
     echo $line
     trline=`echo $line | tr -d '[\r\n]'`
 
     # condition check
     [-z "$trline"] && break
+
+    HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+
+    [[ "$trline" =~ $HEADLINE_REGEX ]] && 
+      REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
   
   done
 
-  echo -e 'HTTP/1.1 200 OK\r\n\r\n\r\n<h1>PONGG</h1>' > response
+  case "$REQUEST" in 
+    "GET / ") RESPONSE="HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n<h1>PONG</h1>" ;;
+           *) RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found"
+    esac
+
+  echo -e $RESPONSE > response
 
 }
 
